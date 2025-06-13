a8a5267285b899b0bd2f790e9e3e1c5877795eb3
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-06-13 23:16:42 +0100

    g

--- webtools/settings.py
+++ webtools/settings.py
@@ -12,7 +12,7 @@ editors: _typing.List[str] = []
 contributors: _typing.List[_typing.Dict[str, str]] = []
 url: _typing.Optional[str] = None
 website_name: _typing.List[_typing.Optional[str]] = [None, None]
-local_prefix: _typin.Optional[str] = None
+local_prefix: _typing.Optional[str] = None
 repo: _typing.Optional[str] = None
 
 re_extras: _typing.List[_typing.Tuple[str, _typing.Callable]] = []
